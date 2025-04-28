4a0415801e269cc9bca84fa1929f16e9f5b93c26
lc025 <leomq05@gmail.com>
2025-04-28 20:50:39 -0300

    Projeto finalizado

--- app/src/main/java/lx025/com/github/cryptomonitor/MainActivity.kt
+++ app/src/main/java/lx025/com/github/cryptomonitor/MainActivity.kt
@@ -1,9 +1,20 @@
 package lx025.com.github.cryptomonitor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import lx025.com.github.cryptomonitor.service.MercadoBitcoinServiceFactory
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         // Configurando a toolbar
         val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
         configureToolbar(toolbarMain)
+
+        val btnRefresh: Button = findViewById(R.id.btn_refresh)
+        btnRefresh.setOnClickListener {
+            makeRestCall()
+        }
+
     }
 
     private fun configureToolbar(toolbar: Toolbar) {
@@ -22,7 +39,47 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setTitle(getText(R.string.app_title))
         supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
 
+    }
 
+    private fun makeRestCall() {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val service = MercadoBitcoinServiceFactory().create()
+                val response = service.getTicker()
 
+                if (response.isSuccessful) {
+                    val tickerResponse = response.body()
+
+                    // Atualizando os componentes TextView
+                    val lblValue: TextView = findViewById(R.id.lbl_value)
+                    val lblDate: TextView = findViewById(R.id.lbl_date)
+
+                    val lastValue = tickerResponse?.ticker?.last?.toDoubleOrNull()
+                    if (lastValue != null) {
+                        val numberFormat = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+                        lblValue.text = numberFormat.format(lastValue)
+                    }
+
+                    val date = tickerResponse?.ticker?.date?.let { Date(it * 1000L) }
+                    val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.getDefault())
+                    lblDate.text = sdf.format(date)
+
+                } else {
+                    // Trate o erro de resposta não bem-sucedida
+                    val errorMessage = when (response.code()) {
+                        400 -> "Bad Request"
+                        401 -> "Unauthorized"
+                        403 -> "Forbidden"
+                        404 -> "Not Found"
+                        else -> "Unknown error"
+                    }
+                    Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_LONG).show()
+                }
+
+            } catch (e: Exception) {
+                // Trate o erro de falha na chamada
+                Toast.makeText(this@MainActivity, "Falha na chamada: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
